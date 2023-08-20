b8b3227367cc084335633593d4c5642ed66875fe
Adam Robson <adamray312@gmail.com>
2023-08-19 19:24:06 -0700

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/my_todo_items'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
